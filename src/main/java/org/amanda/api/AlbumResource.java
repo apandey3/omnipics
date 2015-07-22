bb6980f0f0ef1ce8d02a063d4ad5137f0cf28d54
Arti Sharma <arti.sharma@gmail.com>
2015-07-21 17:27:49 -0700

    dummy method

--- src/main/java/org/amanda/api/AlbumResource.java
+++ src/main/java/org/amanda/api/AlbumResource.java
@@ -1,9 +1,13 @@
 package org.amanda.api;
 
+import org.amanda.Album;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Arti Sharma <arti.sharma@gmail.com>
@@ -13,4 +17,13 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class AlbumResource {
     //TODO: Add a GET method to list albums
+
+    public List<Album> getAlbums(){
+        List <Album> list = whatTodo();
+        return list;
+    }
+
+    private List<Album> whatTodo() {
+        return null;
+    }
 }
